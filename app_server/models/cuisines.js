f49dd617512c5b81a62bf9c0305c6278712bba96
ashimpaudel <ashimpaudel@gmail.com>
2018-02-25 20:24:03 -0500

    added live database

---
+++ app_server/models/cuisines.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+//first we define the schema for subdocuments
+//In our case, reviews and hosting Time are subdocuments
+var reviewSchema = new mongoose.Schema({
+ 	author: String,
+ 	rating: {type: Number, required: true, min: 0, max: 5},
+ 	reviewText: String,
+ 	createdOn: {type: Date, default: Date.now}
+}); 
+
+var hostingTimeSchema = new mongoose.Schema({
+ 	days: {type: String, required: true},
+ 	opening: String,
+ 	closing: String,
+ 	closed: {type: Boolean, required: true}
+});
+
+var cuisineSchema = new mongoose.Schema({
+	name: {type: String, required: true},
+ 	address: String,
+ 	rating: {type: Number, "default": 0, min: 0, max: 5},
+ 	intro: String,
+ 	coords: {type: [Number], index: '2dsphere'},
+ 	hostingTimes: [hostingTimeSchema],
+ 	reviews: [reviewSchema]
+});
