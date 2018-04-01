9ba1508a6b665f492f9bffad5d9a4231ac9c169b
Anuraag Rijal <anuraag.rijal@bison.howard.edu>
2018-03-31 20:57:00 -0400

    Small changes

--- src/app/post-cuisine/cuisine-form/cuisine-form.component.ts
+++ src/app/post-cuisine/cuisine-form/cuisine-form.component.ts
@@ -29,7 +29,6 @@ export class Cuisine {
   templateUrl: './cuisine-form.component.html',
   styleUrls: ['./cuisine-form.component.css']
 })
-<<<<<<< HEAD
 export class CuisineFormComponent implements OnInit {
 	// cuisinesRef: AngularFireList<any>;
 	// cuisines: Observable<any[]>;	
@@ -63,42 +62,3 @@ export class CuisineFormComponent implements OnInit {
 		this.itemsRef.push(this.singleCuisine.value);
   }
 }
-=======
-
-export class CuisineFormComponent {
-	
-
-	cuisinesRef: AngularFireList<any>;
-	cuisines: Observable<any[]>;	
-	submitted: boolean;
-
-  @ViewChild('cuisineForm') singleCuisine: NgForm;
-	 
-	constructor( db: AngularFireDatabase) {	
-		 this.cuisinesRef = db.list('data');
-		 this.cuisines = this.cuisinesRef.valueChanges().map(changes => {
-			 return changes.map(c => ({key: c.payload.key, ...c.payload.val() }))
-		 });
-		 console.log(this.singleCuisine);
-	 }
-
-	 addItem(newName : any) {
-		 this.cuisinesRef.push({ name: newName });
-
-		 }
-		
-
-  onSubmit() { 
-    this.submitted = true;
-		//console.log(this.singleCuisine.value);
-		this.addItem('fdafkldk  ofasoid');	
-		//const itemsRef = this.db.list('data');
-		//itemsRef.push({ });	
-  }
-
-  // ngOnInit() {
-  //   this.submitted = false;
-  // }
-
-}
->>>>>>> b0ac34d1233a9f4f69b411cb385fa153ad28848e
