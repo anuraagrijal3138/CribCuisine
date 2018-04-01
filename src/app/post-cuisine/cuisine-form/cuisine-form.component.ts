b0ac34d1233a9f4f69b411cb385fa153ad28848e
ashimpaudel <ashimpaudel@gmail.com>
2018-03-31 20:06:23 -0400

    update: cuisine form component added

--- src/app/post-cuisine/cuisine-form/cuisine-form.component.ts
+++ src/app/post-cuisine/cuisine-form/cuisine-form.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-// for import { AngularFireDatabase } from 'angularfire2/database';
+ import { Component,  Output, EventEmitter, ViewChild} from '@angular/core';
+
 import { Observable } from 'rxjs/Observable';
-import { AngularFireDatabaseModule, AngularFireList, AngularFireDatabase} from 'angularfire2/database';
+import { AngularFireDatabaseModule, AngularFireObject, AngularFireList, AngularFireDatabase} from 'angularfire2/database';
+
+
+//import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 export class Cuisine {
 
@@ -9,7 +13,7 @@ export class Cuisine {
     public name: string,
     public intro: string,
     public image: string
-  ) {  }
+  ) { }
 
 }
 
@@ -18,25 +22,40 @@ export class Cuisine {
   templateUrl: './cuisine-form.component.html',
   styleUrls: ['./cuisine-form.component.css']
 })
-export class CuisineFormComponent implements OnInit {
-	
-	model  = new Cuisine('name', 'Intor', 'Chuck Overstreet');
-	submitted = false;
 
-	constructor( db: AngularFireDatabase) {		
-			console.log(db);
-			const itemsRef = db.list('data');
-			itemsRef.push({ name: "tried from database" });				
+export class CuisineFormComponent {
+	
+
+	cuisinesRef: AngularFireList<any>;
+	cuisines: Observable<any[]>;	
+	submitted: boolean;
+
+  @ViewChild('cuisineForm') singleCuisine: NgForm;
+	 
+	constructor( db: AngularFireDatabase) {	
+		 this.cuisinesRef = db.list('data');
+		 this.cuisines = this.cuisinesRef.valueChanges().map(changes => {
+			 return changes.map(c => ({key: c.payload.key, ...c.payload.val() }))
+		 });
+		 console.log(this.singleCuisine);
+	 }
+
+	 addItem(newName : any) {
+		 this.cuisinesRef.push({ name: newName });
+
+		 }
 		
-	}
-	onSubmit() {
 
-
-	}
-	
-
-  ngOnInit() {
-		this.submitted = false;	
+  onSubmit() { 
+    this.submitted = true;
+		//console.log(this.singleCuisine.value);
+		this.addItem('fdafkldk  ofasoid');	
+		//const itemsRef = this.db.list('data');
+		//itemsRef.push({ });	
   }
+
+  // ngOnInit() {
+  //   this.submitted = false;
+  // }
 
 }
