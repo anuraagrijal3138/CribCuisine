8d50a6af03b92c9fa2ec26cc4928aa0bdbd92b66
ashimpaudel <ashimpaudel@gmail.com>
2018-04-04 15:23:39 -0400

    current deployment here

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+	firebase: {
+		apiKey: "AIzaSyAwmw91CYCT7zYm6_xIqEA6woiDqB9P8mM",
+    authDomain: "cribcuisine.firebaseapp.com",
+    databaseURL: "https://cribcuisine.firebaseio.com",
+    projectId: "cribcuisine",
+    storageBucket: "cribcuisine.appspot.com",
+    messagingSenderId: "252261267111"
+	}
 };
